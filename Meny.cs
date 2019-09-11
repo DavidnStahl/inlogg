8ddd15516682e91e16979fa9dc150ba6ab8d0931
David Ståhl <47720480+DavidnStahl@users.noreply.github.com>
2019-09-11 17:26:56 +0200

    init

---
+++ Meny.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace filerSparaLösenord
+{
+    class Meny
+    {
+        public int MenyToStart()
+        {
+            Console.WriteLine("Meny: ");
+            Console.WriteLine("a : Logga in ");
+            Console.WriteLine("b : Registrera ");
+            Console.Write("Välj a eller b: ");
+            string userinput = Console.ReadLine();
+
+            if (userinput == "a") { return 1; }
+            if (userinput == "b") { return 2; }
+
+            return 0;
+
+        }
+        public bool LoggaIn(string input)
+        {
+            StreamReader path = new StreamReader(@"C:\Users\dns20\source\repos\filerSparaLösenord\filerSparaLösenord\usernamepassword.txt");
+            bool ok = false;
+            using (path)
+            {
+                string line = path.ReadLine();
+
+                while (line != null)
+                {
+                    line = path.ReadLine();
+
+                    if (line == input) { ok = true; }
+                }
+            }
+
+            return ok;
+
+
+        }
+        public bool RegistreraAnvändarnamn(string input)
+        {
+
+
+            StreamReader path = new StreamReader(@"C:\Users\dns20\source\repos\filerSparaLösenord\filerSparaLösenord\usernamepassword.txt");
+            bool ok = false;
+            using (path)
+            {
+                string line = path.ReadLine();
+
+                while (line != null)
+                {
+                    line = path.ReadLine();
+
+                    if (line == input) { ok = true; }
+                }
+            }
+
+            return ok;
+
+
+        }
+        public bool CheckUserName(string username)
+        {
+            StreamReader path = new StreamReader(@"C:\Users\dns20\source\repos\filerSparaLösenord\filerSparaLösenord\usernamepassword.txt");
+            bool ok = false;
+            using (path)
+            {
+                string line = path.ReadLine();
+
+                while (line != null)
+                {
+                    line = path.ReadLine();
+
+                    if (line == username)
+                    {
+                        ok = true;
+                    }
+                }
+            }
+
+            return ok;
+        }
+        public void AddNewUsernameAndPassword(string input)
+        {
+            string path = @"C:\Users\dns20\source\repos\filerSparaLösenord\filerSparaLösenord\usernamepassword.txt";
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(input);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace filerSparaLösenord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool play = true;
+            while (play)
+            {
+                var startGame = new Meny();
+                int val = startGame.MenyToStart();
+                if (val == 1)
+                {
+                    Console.Write("användarnamn: ");
+                    string username = Console.ReadLine();
+                    Console.Write("password: ");
+                    string password = Console.ReadLine();
+                    string saveinput = (username + " " + password);
+                    if (startGame.LoggaIn(saveinput))
+                    {
+                        Console.WriteLine("Du är inloggad grattis!!");
+                    }
+                    else if (!startGame.LoggaIn(saveinput)) { Console.WriteLine("Fel inloggning uppgifter, försök igen, eller registera nya uppgifter:"); }
+
+                    
+
+
+                }
+                else if (val == 2)
+                {
+                    while (true)
+                    {
+                        Console.Write("Skriv nytt användarnamn: ");
+                        string username = Console.ReadLine();
+                        Console.Write("Skriv nytt Lösenord: ");
+                        string password = Console.ReadLine();
+                        string saveinput = (username + " " + password);
+
+                        if (!startGame.RegistreraAnvändarnamn(saveinput))
+                        {
+                            Console.WriteLine("Din regitrering är klar!!");
+                            startGame.AddNewUsernameAndPassword(saveinput);
+                            break;
+
+                        }
+                        else if (startGame.RegistreraAnvändarnamn(saveinput))
+                        {
+                            Console.WriteLine("Användarnamnet finns redan!! försök igen");
+                        }
+                    }
+                }
+            }
+            
+
+
+
+
+        }
+    }
+}
